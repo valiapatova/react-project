3e5bad3dfb7aa40d3106880043ee00c8b1b75329
ValentinaPatova <valentina.patova@abv.bg>
2023-11-02 12:11:22 +0200

    Undo .HeaderImg.module.css

--- src/components/HeaderImg.jsx
+++ src/components/HeaderImg.jsx
@@ -1,10 +1,10 @@
-import styles from './HeaderImg.module.css';
+
 
 export default function HeaderImg() {
   return (
 
-    <div className={styles["hero_bg_box"]}>
-      <div className={styles["bg_img_box"]}>
+    <div className="hero_bg_box">
+      <div className="bg_img_box">
         <img src="images/hero-bg.png" alt="" />
       </div>
     </div>
