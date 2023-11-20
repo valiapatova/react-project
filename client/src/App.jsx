b698ffe5be379819579f40753a097b8d83ba7f14
Valentina Patova-Staninska <102672076+valiapatova@users.noreply.github.com>
2023-11-20 13:04:15 +0200

    Spread components in different folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import { Routes, Route } from 'react-router-dom';
 import NavigationMenuDark from './components/navigationMenu/NavigationMenuDark.jsx';
 
 import HeaderHome from './components/home/HeaderHome.jsx';
-import OurServices from './components/OurServices';
-import About from './components/About';
+import OurServices from './components/ourServices/OurServices.jsx';
+import About from './components/about/About.jsx';
 import WhyUs from './components/catalogWhyUs/WhyUs.jsx';
 import WhyUsDetails from './components/catalogWhyUs/WhyUsDetails.jsx';
 import Team from './components/Team';
