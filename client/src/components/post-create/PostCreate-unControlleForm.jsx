373788529cf23c1a74937769a58420e638e0ccc2
Valentina Patova-Staninska <102672076+valiapatova@users.noreply.github.com>
2023-12-06 19:01:14 +0200

    Migrate to Controlled form in PostCreate.jsx

---
+++ client/src/components/post-create/PostCreate-unControlleForm.jsx
@@ -0,0 +1,75 @@
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+
+import AuthContext from '../../contexts/authContex.jsx';
+
+import * as postService from '../../services/postService.js';
+
+
+import styles from './PostCreate.module.css';
+
+export default function PostCreate() {
+
+    const{errorHandler}=useContext(AuthContext);
+
+    const navigate = useNavigate();
+
+    const createPostSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        const postData = Object.fromEntries(new FormData(e.currentTarget));
+
+        try {
+            // throw Error("Моята предизвикана грешка");
+            
+            await postService.create(postData);
+            navigate('/posts');
+
+        } catch (err) {
+            // Error notification
+            console.log(err);
+            errorHandler(err);
+        }
+    }
+
+    return (
+
+        <div className={styles.heroBg_box}>
+
+            <div className={styles.create_page}>
+
+                {/* <div className={styles.heroBg_box}> */}
+
+                    <section id="create-page" className={styles.auth}>
+                        <form id="create" onSubmit={createPostSubmitHandler}>
+                            <div className={styles.containerCreate}>
+                                <h2>Създай карта</h2>
+                                <label className={styles.label} htmlFor="title">Име:</label>
+                                <input className={styles.inputText} type="text" id="title" name="title" placeholder="трите имена..." />
+
+                                <label className={styles.label} htmlFor="category">Фирма:</label>
+                                <input className={styles.inputText} type="text" id="category" name="category" placeholder="фирма,работодател..." />
+
+                                <label className={styles.label} htmlFor="maxLevel">Възраст:</label>
+                                <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" className={styles.inputText} />
+
+                                <label className={styles.label} htmlFor="imageUrl">Снимка:</label>
+                                <input className={styles.inputText} type="text" id="imageUrl" name="imageUrl" placeholder="качи снимка..." />
+
+                                <label className={styles.label} htmlFor="summary">Резюме:</label>
+                                <textarea name="summary" id="summary"></textarea>
+
+                                <input className={styles.btn_submit} type="submit" value="Създай карта" />
+                            </div>
+                        </form>
+                    </section>
+
+                {/* </div> */}
+            </div>
+        </div>
+
+
+
+
+    );
+}
--- client/src/components/post-create/PostCreate.jsx
+++ client/src/components/post-create/PostCreate.jsx
@@ -2,27 +2,49 @@ import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 
 import AuthContext from '../../contexts/authContex.jsx';
+import useForm from '../../hooks/useForm.js';
 
 import * as postService from '../../services/postService.js';
 
 
 import styles from './PostCreate.module.css';
 
+
+// const initialFormValuesState = {
+//         title: '',
+//         category: '',
+//         maxLevel: '',
+//         imageUrl: '',
+//         summary: '',
+//     }
+
+const CreatePostFormKeys = {
+
+    Title: "title",
+    Category: "category",
+    MaxLevel: "maxLevel",
+    ImageUrl: "imageUrl",
+    Summary: "summary",
+}
+
+
+
+
 export default function PostCreate() {
 
-    const{errorHandler}=useContext(AuthContext);
+    const { errorHandler } = useContext(AuthContext);
 
     const navigate = useNavigate();
 
-    const createPostSubmitHandler = async (e) => {
-        e.preventDefault();
+    const createPostSubmitHandler = async (values) => {
 
-        const postData = Object.fromEntries(new FormData(e.currentTarget));
+        //e.preventDefault();
+        //const postData = Object.fromEntries(new FormData(e.currentTarget));
 
         try {
             // throw Error("Моята предизвикана грешка");
-            
-            await postService.create(postData);
+
+            await postService.create(values);
             navigate('/posts');
 
         } catch (err) {
@@ -32,6 +54,22 @@ export default function PostCreate() {
         }
     }
 
+    // const initialFormValuesState = {
+    //     title: '',
+    //     category: '',
+    //     maxLevel: '',
+    //     imageUrl: '',
+    //     summary: '',
+    // }
+
+    const { values, onChange, onSubmit, onReset } = useForm(createPostSubmitHandler, {
+        [CreatePostFormKeys.Title]: '',
+        [CreatePostFormKeys.Category]: '',
+        [CreatePostFormKeys.MaxLevel]: '',
+        [CreatePostFormKeys.ImageUrl]: '',
+        [CreatePostFormKeys.Summary]: '',
+    });
+
     return (
 
         <div className={styles.heroBg_box}>
@@ -40,36 +78,80 @@ export default function PostCreate() {
 
                 {/* <div className={styles.heroBg_box}> */}
 
-                    <section id="create-page" className={styles.auth}>
-                        <form id="create" onSubmit={createPostSubmitHandler}>
-                            <div className={styles.containerCreate}>
-                                <h2>Създай карта</h2>
-                                <label className={styles.label} htmlFor="title">Име:</label>
-                                <input className={styles.inputText} type="text" id="title" name="title" placeholder="трите имена..." />
+                <section id="create-page" className={styles.auth}>
 
-                                <label className={styles.label} htmlFor="category">Фирма:</label>
-                                <input className={styles.inputText} type="text" id="category" name="category" placeholder="фирма,работодател..." />
+                    <form id="create" onSubmit={onSubmit}>
 
-                                <label className={styles.label} htmlFor="maxLevel">Възраст:</label>
-                                <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" className={styles.inputText} />
+                        <div className={styles.containerCreate}>
+                            <h2>Създай карта</h2>
+                            <label className={styles.label} htmlFor="title">Име:</label>
+                            <input
+                                className={styles.inputText}
+                                type="text"
+                                id="title"
+                                name={CreatePostFormKeys.Title}
+                                value={values[CreatePostFormKeys.Title]}
+                                onChange={onChange}
+                                placeholder="трите имена..."
+                            />
 
-                                <label className={styles.label} htmlFor="imageUrl">Снимка:</label>
-                                <input className={styles.inputText} type="text" id="imageUrl" name="imageUrl" placeholder="качи снимка..." />
+                            <label className={styles.label} htmlFor="category">Фирма:</label>
+                            <input
+                                className={styles.inputText}
+                                type="text"
+                                id="category"
+                                name={CreatePostFormKeys.Category}
+                                value={values[CreatePostFormKeys.Category]}
+                                onChange={onChange}
+                                placeholder="фирма,работодател..."
+                            />
 
-                                <label className={styles.label} htmlFor="summary">Резюме:</label>
-                                <textarea name="summary" id="summary"></textarea>
+                            <label className={styles.label} htmlFor="maxLevel">Възраст:</label>
+                            <input
+                                className={styles.inputText}
+                                type="number"
+                                id="maxLevel"
+                                name={CreatePostFormKeys.MaxLevel}
+                                min="1"
+                                placeholder="1"
+                                value={values[CreatePostFormKeys.MaxLevel]}
+                                onChange={onChange}
+                            />
 
-                                <input className={styles.btn_submit} type="submit" value="Създай карта" />
-                            </div>
-                        </form>
-                    </section>
+                            <label className={styles.label} htmlFor="imageUrl">Снимка:</label>
+                            <input
+                                className={styles.inputText}
+                                type="text"
+                                id="imageUrl"
+                                name={CreatePostFormKeys.ImageUrl}
+                                value={values[CreatePostFormKeys.ImageUrl]}
+                                onChange={onChange}
+                                placeholder="качи снимка..."
+                            />
+
+                            <label className={styles.label} htmlFor="summary">Резюме:</label>
+                            <textarea
+                                name={CreatePostFormKeys.Summary}
+                                id="summary"
+                                value={values[CreatePostFormKeys.Summary]}
+                                onChange={onChange}
+                            >
+                            </textarea>
+
+                            <input
+                             className={styles.btn_submit}
+                             type="submit"
+                             value="Създай карта"
+                            />
+
+                        </div>
+                    </form>
+                    
+                </section>
 
                 {/* </div> */}
             </div>
         </div>
-
-
-
 
     );
 }
